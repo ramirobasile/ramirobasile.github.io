8ea65f9588bd112d50b961a953c0c62e9e9244dd
Ramiro Basile <ramirobasile1@gmail.com>
2021-01-07 17:42:36 -0300

    Arreglar for y simplificar

--- publish.sh
+++ publish.sh
@@ -4,35 +4,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# TODO arreglar for
-# TODO comentarios
 for FILE in $(find $@ -maxdepth 1 -type f -name "*.md" -exec realpath {} \;); do
-	ROOT=$(realpath --relative-to=$(dirname "$FILE") .)
+	DIR=$(dirname "$FILE")
 	OUT=$(dirname "$FILE")/$(basename "$FILE" '.md').html
-	
-	cd $(dirname "$FILE")
-	
-	ARGS=()
-	if [ -f "./resources/head.html" ]; then
-		ARGS+=( "-H" "./resources/head.html" )
-	elif [ -f "$ROOT/resources/head.html" ]; then
-		ARGS+=( "-H" "$ROOT/resources/head.html" )
-	fi
-	if [ -f "./resources/header.html" ]; then
-		ARGS+=( "--include-before-body" "./resources/header.html" )
-	elif [ -f "$ROOT/resources/header.html" ]; then
-		ARGS+=( "--include-before-body" "$ROOT/resources/header.html" )
-	fi
-	if [ -f "./resources/footer.html" ]; then
-		ARGS+=( "--include-after-body" "./resources/footer.html" )
-	elif [ -f "$ROOT/resources/footer.html" ]; then
-		ARGS+=( "--include-after-body" "$ROOT/resources/footer.html" )
-	fi
 	
 	pandoc $FILE \
 		--to html4 \
 		--email-obfuscation=references \
-		"${ARGS[@]}" \
+		-H resources/head.html \
+		--include-before-body "$DIR"/resources/header.html \
+		--include-after-body resources/footer.html \
 		-o $OUT
 		
 	echo Published $(basename "$FILE") to $(basename "$OUT")
